fb84af87a30b2daa153b69919676e5ed57de815e
Andrei <queyenth@gmail.com>
2021-05-26 15:06:28 +0600

    Add function to wrap value in maybe

--- src/functions.php
+++ src/functions.php
@@ -3,8 +3,18 @@ declare(strict_types=1);
 
 namespace Q\FP;
 
+use Q\FP\Monad\Maybe;
+
 function id ($x) {
     return $x;
+}
+
+function maybe($value): Maybe {
+    if (is_null($value)) {
+        return new Maybe\Nothing;
+    }
+
+    return new Maybe\Just($value);
 }
 
 function comp(...$fns) {
